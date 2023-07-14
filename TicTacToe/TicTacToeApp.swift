cf408402011fa19c5080854e0f7d6cb14a69638e
Vladimir Fibe <motiw@icloud.com>
2023-07-14 19:24:23 +0600

    Initial Commit

---
+++ TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by Vladimir Fibe on 14.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
